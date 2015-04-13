6c33bbc307c72a28de11562568cd8714ea6e4b27
sonSunnoi <whoknowtrueofearth@gmail.com>
2015-04-13 08:09:28 +0700

    Update Knife.java

--- src/com/talesdev/copsandcrims/guns/Knife.java
+++ src/com/talesdev/copsandcrims/guns/Knife.java
@@ -14,7 +14,7 @@ import org.bukkit.inventory.ItemStack;
 public class Knife extends Weapon {
     public Knife() {
         super("Knife", "Knife", blankAliases(), WeaponType.MELEE);
-        MeleeModule meleeModule = new MeleeModule(4, 8);
+        MeleeModule meleeModule = new MeleeModule(5, 15, 30, 75);
         addModule(meleeModule);
     }
 
