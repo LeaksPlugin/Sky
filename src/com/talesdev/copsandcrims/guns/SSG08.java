b07ca58855190af11ac78654a7769df1edccfaf6
sonSunnoi <whoknowtrueofearth@gmail.com>
2015-04-14 19:26:37 +0700

    Create SSG08.java

---
+++ src/com/talesdev/copsandcrims/guns/SSG08.java
@@ -0,0 +1,66 @@
+package com.talesdev.copsandcrims.guns;
+
+import com.talesdev.copsandcrims.weapon.Weapon;
+import com.talesdev.copsandcrims.weapon.WeaponType;
+import com.talesdev.copsandcrims.weapon.bullet.Accuracy;
+import com.talesdev.copsandcrims.weapon.bullet.BulletAccuracy;
+import com.talesdev.copsandcrims.weapon.module.*;
+import com.talesdev.core.math.Range;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+/**
+ * SSG 08
+ *
+ * @author sonSunnoi
+ */
+public class SSG08 extends Weapon {
+
+    public SSG08() {
+        super("SSG08", ChatColor.GREEN + "SSG 08", blankAliases(), WeaponType.SNIPER_RIFLE);
+        ShootingModule shootingModule = new ShootingModule();
+        ScopeModule scopeModule = new ScopeModule();
+        ItemControlModule controlModule = new ItemControlModule();
+        DeathMessageModule deathMessageModule = new DeathMessageModule();
+        shootingModule.setDamage(17.6);
+        shootingModule.setHeadShotDamage(47.1);
+        shootingModule.setRecoil(15.0D);
+        shootingModule.setMaxBullet(10);
+        shootingModule.setBulletDelay(16);
+        shootingModule.setCooldownTime(16);
+        shootingModule.setReloadTime(74);
+        shootingModule.setAccuracy(new BulletAccuracy(
+                new Accuracy(new Range(-80, 80), new Range(-80, 80), new Range(-80, 80)), // default
+                new Accuracy(new Range(0, 0), new Range(0, 0), new Range(0, 0)), // sneaking
+                new Accuracy(new Range(-20, 20), new Range(-20, 20), new Range(-20, 20)), // walking
+                new Accuracy(new Range(-80, 80), new Range(-80, 80), new Range(-80, 80)), // sprinting
+                new Accuracy(new Range(-100, 100), new Range(-100, 100), new Range(-100, 100)) // jumping
+        ));
+        shootingModule.setFiringMode(FiringMode.BOLT);
+        scopeModule.setZoomLevel(5);
+        addModule(shootingModule);
+        addModule(controlModule);
+        addModule(deathMessageModule);
+        addModule(scopeModule);
+    }
+
+    @Override
+    public ItemStack createItemStack() {
+        ItemStack itemStack = new ItemStack(Material.DIAMOND_PICKAXE, 10);
+        ItemMeta meta = itemStack.getItemMeta();
+        meta.setDisplayName(ChatColor.GREEN + getDisplayName());
+        itemStack.setItemMeta(meta);
+        return itemStack;
+    }
+
+    @Override
+    public boolean isWeapon(ItemStack itemStack) {
+        if (itemStack == null) return false;
+        if (itemStack.getType().equals(Material.DIAMOND_PICKAXE)) {
+            return true;
+        }
+        return super.isWeapon(itemStack);
+    }
+}
