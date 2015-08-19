37d5fc0b2d53abac0118c6fa40cf379e76e66f1d
sonSunnoi <whoknowtrueofearth@gmail.com>
2015-08-19 15:55:27 +0700

    Update Knife.java

--- src/com/talesdev/copsandcrims/guns/Knife.java
+++ src/com/talesdev/copsandcrims/guns/Knife.java
@@ -17,7 +17,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class Knife extends Weapon {
     public Knife() {
         super("Knife", ChatColor.GREEN + "Knife", blankAliases(), WeaponType.MELEE);
-        MeleeModule meleeModule = new MeleeModule(5, 15, 30, 75);
+        MeleeModule meleeModule = new MeleeModule(5, 15, 25, 50);
         DeathMessageModule messageModule = new DeathMessageModule();
         addModule(messageModule);
         addModule(meleeModule);
